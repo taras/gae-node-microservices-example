54c16086fa42f0646593062b9d1a901be9fd69e1
Taras Mankovski <tarasm@gmail.com>
2017-01-10 20:05:59 -0500

    Dispatcher is working for both subdomain and directory

--- service-1/app.js
+++ service-1/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/service-1/hello', (req, res) => {
+app.get('/', (req, res) => {
     res.status(200).send('Service 1: Hello, world!');
 });
 
--- service-2/app.js
+++ service-2/app.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/service-2/hello', (req, res) => {
+app.get('/service-2/', (req, res) => {
     res.status(200).send('Service 2: Hello, world!');
 });
 
